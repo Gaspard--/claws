6e277b8d4ce9e19115ba6a279b0a5ce494485e7f
Gaspard-- <jakob.kellendonk@epitech.eu>
2018-02-11 17:51:09 +0100

    added onScopeExit function

---
+++ OnScopeExit.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <optional>
+
+namespace claws
+{
+  template<class Func>
+  auto onScopeExit(Func func)
+  {
+    struct Ret
+    {
+      std::optional<Func> func;
+
+      Ret(Func func)
+	: func(std::make_optional(func))
+      {
+      }
+
+      Ret(Ret const &ret) = delete;
+
+      Ret(Ret &&ret)
+	: func(ret.func)
+      {
+	ret.func.reset();
+      };
+
+      ~Ret()
+      {
+	if (func)
+	  (*func)();
+      }
+    };
+
+    return Ret{func};
+  }
+};
