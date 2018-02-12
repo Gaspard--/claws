5726031ab0c2e67f3943ddb441d75f4a035b8922
Gaspard-- <jakob.kellendonk@epitech.eu>
2018-02-12 15:10:37 +0100

    added constexpr copy

---
+++ ConstexprAlgorithm.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace claws
+{
+  template<class InputIt, class OutputIt>
+  constexpr OutputIt copy(InputIt begin, InputIt end, OutputIt out)
+  {
+    while (begin != end)
+      {
+	*out = *begin;
+	++begin;
+	++out;
+      }
+    return out;
+  }
+};
