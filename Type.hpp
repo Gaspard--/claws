24729cc8a9941859e223c50fec404d4cd50b1bd9
Gaspard-- <jakob.kellendonk@epitech.eu>
2018-02-12 15:10:58 +0100

    added parametter pack copying facility

---
+++ Type.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <tuple>
+
+namespace claws
+{
+  template<template<class...> class DestTemplate, class Src>
+  class CopyParamPack
+  {
+    template<class... T,  template<class...> class SrcTemplate>
+    static DestTemplate<T...> convert(SrcTemplate<T...>);
+
+  public:
+    using Type = decltype(convert(std::declval<Src>()));
+  };
+
+  template<template<class...> class DestTemplate, class Src>
+  using CopyParamPack_T = typename CopyParamPack<DestTemplate, Src>::Type;
+  
+  template<class Src>
+  using ToTuple = CopyParamPack<std::tuple, Src>;
+
+  template<class Src>
+  using ToTuple_T = typename ToTuple<Src>::Type;
+}
