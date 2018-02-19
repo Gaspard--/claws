5630fc711594f7dcbeb092e7035a582a62dc4830
Gaspard-- <jakob.kellendonk@epitech.eu>
2018-02-19 12:59:23 +0100

    added constexpr on lambda ops

--- LambdaOps.hpp
+++ LambdaOps.hpp
@@ -7,7 +7,7 @@ namespace claws
     template<class Op1, class Op2>
     struct Overload : private Op1, private Op2
     {
-      Overload(Op1 lh, Op2 rh)
+      constexpr Overload(Op1 lh, Op2 rh)
 	: Op1(lh), Op2(rh)
       {
       }
@@ -16,7 +16,7 @@ namespace claws
     };
 
     template<class Op1, class Op2>
-    auto operator+(Op1 lh, Op2 rh)
+    constexpr auto operator+(Op1 lh, Op2 rh)
     {
       return Overload<Op1, Op2>{lh, rh};
     }
@@ -24,26 +24,26 @@ namespace claws
     template<class Op1, class Op2>
     struct Composition : private Op1, private Op2
     {
-      Composition(Op1 lh, Op2 rh)
+      constexpr Composition(Op1 lh, Op2 rh)
 	: Op1(lh), Op2(rh)
       {
       }
 
       template<class... Params>
-      auto operator()(Params &&... params)
+      constexpr auto operator()(Params &&... params)
       {
 	return Op1::operator()(Op2::operator()(std::forward<Params>(params)...));
       }
 
       template<class... Params>
-      auto operator()(Params &&... params) const
+      constexpr auto operator()(Params &&... params) const
       {
 	return Op1::operator()(Op2::operator()(std::forward<Params>(params)...));
       }
     };
 
     template<class Op1, class Op2>
-    auto operator*(Op1 lh, Op2 rh)
+    constexpr auto operator*(Op1 lh, Op2 rh)
     {
       return Composition<Op1, Op2>{lh, rh};
     }
