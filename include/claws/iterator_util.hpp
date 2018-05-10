a4d0b2a09a8f2f2a38e0aae77cd09f6e3ad846aa
Gaspard-- <jakob.kellendonk@epitech.eu>
2018-05-10 20:26:02 +0200

    Added constructors, changed class storage system
    The container_view will now store a temporary it recieves through the constructor.
    It it recieves a const referencce, it will store begin and end instead.
    
    Added:
    - new constructor
    - dedution guidelines
    
    Modified:
    - class template parameters
    - class storage and access members

--- include/claws/iterator_util.hpp
+++ include/claws/iterator_util.hpp
@@ -47,22 +47,22 @@ namespace claws
       return tmp;
     }
 
-#define CONTAINER_VIEW_ITERATOR_OP(OP)                                                                                                                         \
-  constexpr iterator_view &operator OP##=(difference_type val) noexcept(noexcept(it OP## = std::declval<difference_type>()))                                   \
-  {                                                                                                                                                            \
-    it OP## = val;                                                                                                                                             \
-    return *this;                                                                                                                                              \
-  };                                                                                                                                                           \
-                                                                                                                                                               \
-  constexpr iterator_view operator OP(difference_type val)                                                                                                     \
-    const noexcept(noexcept(std::declval<iterator_view<raw_it_type, func_type>>() OP## = std::declval<difference_type>())                                      \
-                   && std::is_nothrow_copy_constructible_v<iterator_view<raw_it_type, func_type>>)                                                             \
-  {                                                                                                                                                            \
-    auto tmp(*this);                                                                                                                                           \
-                                                                                                                                                               \
-    tmp OP## = val;                                                                                                                                            \
-    return tmp;                                                                                                                                                \
-  };
+#define CONTAINER_VIEW_ITERATOR_OP(OP)					\
+    constexpr iterator_view &operator OP##=(difference_type val) noexcept(noexcept(it OP## = std::declval<difference_type>())) \
+    {									\
+      it OP## = val;							\
+      return *this;							\
+    };									\
+									\
+    constexpr iterator_view operator OP(difference_type val)		\
+      const noexcept(noexcept(std::declval<iterator_view<raw_it_type, func_type>>() OP## = std::declval<difference_type>()) \
+		     && std::is_nothrow_copy_constructible_v<iterator_view<raw_it_type, func_type>>) \
+    {									\
+      auto tmp(*this);							\
+									\
+      tmp OP## = val;							\
+      return tmp;							\
+    };
 
     CONTAINER_VIEW_ITERATOR_OP(-);
     CONTAINER_VIEW_ITERATOR_OP(+);
@@ -98,11 +98,11 @@ namespace claws
       return self_pointer_like{func(*it)};
     }
 
-#define CONTAINER_VIEW_ITERATOR_BINARY_PREDICATE(OP)                                                                                                           \
-  constexpr bool operator OP(iterator_view other) const noexcept(noexcept(it OP std::declval<raw_it_type>()))                                                  \
-  {                                                                                                                                                            \
-    return it OP other.it;                                                                                                                                     \
-  }
+#define CONTAINER_VIEW_ITERATOR_BINARY_PREDICATE(OP)			\
+    constexpr bool operator OP(iterator_view other) const noexcept(noexcept(it OP std::declval<raw_it_type>()))	\
+    {									\
+      return it OP other.it;						\
+    }
 
     /// \name comparision operators
     /// comparaisions are inherited from
@@ -116,15 +116,28 @@ namespace claws
     /// @}
   };
 
-  template<class it_type, class end_type, class func_type>
-  class container_view
+  template<class it_type, class end_type>
+  struct iterator_pair
   {
     it_type _begin;
     end_type _end;
-    func_type _func;
 
-    static constexpr bool constructors_are_noexcept{
-      std::is_nothrow_copy_constructible_v<it_type> && std::is_nothrow_copy_constructible_v<end_type> && std::is_nothrow_copy_constructible_v<func_type>};
+    auto begin() const noexcept(std::is_nothrow_copy_constructible_v<it_type>)
+    {
+      return _begin;
+    }
+
+    auto end() const noexcept(std::is_nothrow_copy_constructible_v<end_type>)
+    {
+      return _end;
+    }
+  };
+
+  template<class container_type, class func_type>
+  class container_view
+  {
+    container_type container;
+    func_type _func;
 
   public:
     /// \name constructors and assignement operators
@@ -140,27 +153,43 @@ namespace claws
     /// ```cpp
     /// for (auto value : container_view(begin, end, func))
     /// ```
-    /// which is really usefull. It may be necessary to qualify the auto properly depending on use-cases.
+    /// which is really useful. It may be necessary to qualify the auto properly depending on use-cases.
     ///
-    constexpr container_view(it_type const &begin, end_type const &end, func_type const &func) noexcept(constructors_are_noexcept)
-      : _begin(begin)
-      , _end(end)
+    template<class it_type, class end_type>
+    constexpr container_view(it_type const &begin, end_type const &end, func_type const &func)
+    : container{begin, end}
       , _func(func)
     {}
 
     ///
     /// \brief contructs a container view
     ///
-    /// This is the constructor that should fit most use cases.
+    /// This is the constructor that should fit most use cases. Not that the container isn't stored, only it's begin(), and end().
+    /// If these get invalidated, the view will be too.
     /// Thanks to the added deduction guidelines, it's possible to write
     /// ```cpp
     /// for (auto value : container_view(container, func))
     /// ```
-    /// which is really usefull. It may be necessary to qualify the auto properly depending on use-cases.
+    /// which is really useful. It may be necessary to qualify the auto properly depending on use-cases.
     ///
-    template<class container_type>
-    constexpr container_view(container_type const &container, func_type const &func) noexcept(constructors_are_noexcept)
+    constexpr container_view(container_type const &container, func_type const &func)
       : container_view(container.begin(), container.end(), func)
+    {}
+
+    ///
+    /// \brief contructs a container view
+    ///
+    /// This constructor stores the recieved container. This gives the useer more flexibillity.
+    /// The parameter will be moved from.
+    /// Thanks to the added deduction guidelines, it's possible to write
+    /// ```cpp
+    /// for (auto value : container_view(create_container(), func))
+    /// ```
+    /// which is really useful. It may be necessary to qualify the auto properly depending on use-cases.
+    ///
+    constexpr container_view(container_type &&container, func_type const &func)
+    : container(std::move(container))
+      , _func(func)
     {}
 
     constexpr container_view() = default;
@@ -180,14 +209,14 @@ namespace claws
     ///
     /// Return `iterator_view`s constructed with func and the corresponding iterator.
     /// @{
-    constexpr auto begin() const noexcept(noexcept(iterator_view(_begin, _func)))
+    constexpr auto begin() const noexcept(noexcept(iterator_view(container.begin(), _func)))
     {
-      return iterator_view(_begin, _func);
+      return iterator_view(container.begin(), _func);
     }
 
-    constexpr auto end() const noexcept(noexcept(iterator_view(_end, _func)))
+    constexpr auto end() const noexcept(noexcept(iterator_view(container.end(), _func)))
     {
-      return iterator_view(_end, _func);
+      return iterator_view(container.end(), _func);
     }
     /// @}
   };
@@ -246,5 +275,14 @@ namespace claws
   // Deduction guideline for second constructor
   template<class container_type, class func_type>
   container_view(container_type const &container, func_type const &func)
-    -> container_view<decltype(container.begin()), decltype(container.begin()), func_type>;
+    -> container_view<iterator_pair<decltype(container.begin()), decltype(container.begin())>, func_type>;
+
+  template<class container_type, class func_type>
+  container_view(container_type &&container, func_type const &func)
+    -> container_view<container_type, func_type>;
+
+  template<class it_type, class end_type, class func_type>
+  container_view(it_type const &begin, end_type const &end, func_type const &func)
+    -> container_view<iterator_pair<it_type, end_type>, func_type>;
+
 }
