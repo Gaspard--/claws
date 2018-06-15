2abdafcf168bc859d0f37618cc5997eb35f6b2a2
Gaspard-- <jakob.kellendonk@epitech.eu>
2018-06-16 00:45:28 +0200

    Added some documentation

--- include/claws/tagged_data.hpp
+++ include/claws/tagged_data.hpp
@@ -2,16 +2,38 @@
 
 namespace claws
 {
+  /// \brief typify a type, helping with type safety
+  /// \tparam _data_type the stored type
+  /// \tparam _offset_type used for offsets (operators `+=`, `+`, `-=`, `-`)
+  /// \tparam _tag a struct used as tag
+  ///
+  /// Stores a `tagged_data::data_type` value.
+  /// If `offset_type` is the same as `data_type` this behaves like an index
+  /// If `offset_type` is not the same as `data_type` this behaves like a pointer
+  /// This class can't be assigned a `data_type` by default,
+  /// helping with accidental promotion
+  /// It should be typically used to help not making mistakes when handling
+  /// different indexes or pointers of the same underlying type.
+  /// The underling type should be cheap to copy and assign,
+  /// and never throw in such operations. (Ideally, a primitive type)
+  ///
   template<class _data_type, class _offset_type, class _tag>
   struct tagged_data
   {
+    /// the used as tag
     using tag = _tag;
+    /// used for offsets (operators `+=`, `+`, `-=`, `-`)
     using offset_type = _offset_type;
+    /// the stored type
     using data_type = _data_type;
+    /// \brief the actual data
+    /// access this when you need the actual value (typically inside your api)
     data_type data;
 
     constexpr tagged_data(tagged_data const &) noexcept = default;
+    constexpr tagged_data& operator=(tagged_data const &) noexcept = default;
 
+    /// Construtor is explicit, to avoid conversion
     explicit constexpr tagged_data(data_type data = 0u) noexcept
       : data(data)
     {}
