57a5822a1e7ce3beab85b83646f2e5a83e3e0543
Doom <clement.doumergue@epitech.eu>
2018-09-07 12:22:22 -0400

    Fix access specifiers and member names in claws::self_inject

--- modules/utils/claws/utils/lambda_utils.hpp
+++ modules/utils/claws/utils/lambda_utils.hpp
@@ -11,32 +11,40 @@ namespace claws
   /// This class takes a lambda as parameter, and its `operator()` injects
   /// the class itself into the lambda as first parameter,
   /// thus allowing the lambda to call the first of it's parameters to call itself
-  /// This class is usable with any functor, even though the readablity gain only really applies to lambdas
+  /// This class is usable with any functor, even though the readability gain only really applies to lambdas
   ///
   /// \tparam functor_type the functor's type
   template<class functor_type>
-  struct inject_self
+  class inject_self
   {
-    functor_type functor;
+  private:
+    functor_type wrapped_functor;
 
+  public:
     inject_self() = default;
     inject_self(inject_self const &) = default;
     inject_self(inject_self &&) = default;
     inject_self &operator=(inject_self const &) = default;
     inject_self &operator=(inject_self &&) = default;
 
-    constexpr inject_self(functor_type const &f) noexcept(std::is_nothrow_copy_constructible_v<functor_type>)
-      : functor(f)
+    constexpr inject_self(functor_type const &functor) noexcept(std::is_nothrow_copy_constructible_v<functor_type>)
+      : wrapped_functor(functor)
     {}
 
-    constexpr inject_self(functor_type &&f) noexcept(std::is_nothrow_move_constructible_v<functor_type>)
-      : functor(std::move(f))
+    constexpr inject_self(functor_type &&functor) noexcept(std::is_nothrow_move_constructible_v<functor_type>)
+      : wrapped_functor(std::move(functor))
     {}
 
     template<class... param_types>
     constexpr decltype(auto) operator()(param_types &&... params) const
     {
-      return functor(*this, std::forward<param_types>(params)...);
+      return wrapped_functor(*this, std::forward<param_types>(params)...);
+    }
+
+    template<class... param_types>
+    decltype(auto) operator()(param_types &&... params)
+    {
+      return wrapped_functor(*this, std::forward<param_types>(params)...);
     }
   };
 };
